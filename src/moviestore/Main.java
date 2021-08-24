ecc0400df8d447141c5a31e6a2f7cf78fd08f097
Chris McColgan <mccolganchrisj@gmail.com>
2021-08-24 10:36:17 -0500

    first commit

---
+++ src/moviestore/Main.java
@@ -0,0 +1,12 @@
+package moviestore;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Customer customer = new Customer("Bob");
+        customer.addRental(new Rental(new Movie("Jaws", Movie.REGULAR), 2));
+        customer.addRental(new Rental(new Movie("Golden Eye", Movie.REGULAR), 3));
+        customer.addRental(new Rental(new Movie("Short New", Movie.NEW_RELEASE), 1));
+        System.out.println(customer.statement());
+    }
+}
